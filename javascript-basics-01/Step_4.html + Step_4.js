239140742a631f4a902967457ce189e1b9f0cdab
Abdullatif-Fk <abul647.fk@gmail.com>
2020-09-28 12:38:42 +0300

    basic-1 level-4

---
+++ javascript-basics-01/Step_4.html + Step_4.js
@@ -0,0 +1,14 @@
+/*var name=prompt("What's your name?");
+var surname=prompt("What's your surname?");
+var city=prompt("What's your city?");
+alert("Nom: "+name+"\n"+"surname: "+surname+"\n"+"City: "+city);*/
+
+
+function validat(){
+     var name=document.getElementById("name").value;
+    var surname=document.getElementById("surname").value;
+    var city=document.getElementById("city").value;
+    
+        alert("Nom: "+name+"\n"+"surname: "+surname+"\n"+"City: "+city);
+
+}
