3b07c4623071699dd080e20bfe9f58273fb2ab9c
Abdullatif-Fk <abul647.fk@gmail.com>
2020-09-29 17:54:05 +0300

    basic02 step_6

---
+++ javascript-basics-02/step 6/index.html + Step_6.js
@@ -0,0 +1,58 @@
+function chagePic(){
+    var image1= document.getElementById("image1");
+    image1.onmouseover=function () {
+        image1.src="images/image1_2.jpg";
+        
+    }
+    var image2= document.getElementById("image2");
+    image2.onmouseover=function () {
+        image2.src="images/image2_2.jpg";
+        
+    }
+    var image3= document.getElementById("image3");
+    image3.onmouseover=function () {
+        image3.src="images/image3_2.jpg";
+        
+    }
+    var image4= document.getElementById("image4");
+    image4.onmouseover=function () {
+        image4.src="images/image4_2.jpg";
+        
+    }
+    var image5= document.getElementById("image5");
+    image5.onmouseover=function () {
+        image5.src="images/image5_2.jpg";
+        
+    }
+ }
+
+ function returnPic(){
+    var image1= document.getElementById("image1");
+    image1.onmouseout=function () {
+        image1.src="images/image1.jpg";
+        
+    }
+    var image2= document.getElementById("image2");
+    image2.onmouseout=function () {
+        image2.src="images/image2.jpg";
+        
+    }
+    var image3= document.getElementById("image3");
+    image3.onmouseout=function () {
+        image3.src="images/image3.jpg";
+        
+    }
+    var image4= document.getElementById("image4");
+    image4.onmouseout=function () {
+        image4.src="images/image4.jpg";
+        
+    }
+    var image5= document.getElementById("image5");
+    image5.onmouseout=function () {
+        image5.src="images/image5.jpg";
+        
+    }
+ 
+ 
+ 
+ }
