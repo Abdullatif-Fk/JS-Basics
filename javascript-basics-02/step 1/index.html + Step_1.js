f7337c82a29bae0a27ebc9729b6ebe6270c1b125
Abdullatif-Fk <abul647.fk@gmail.com>
2020-09-29 15:12:55 +0300

    basic02 step1

---
+++ javascript-basics-02/step 1/index.html + Step_1.js
@@ -0,0 +1,6 @@
+function changePic1() {
+    document.querySelector("#image1").src = "images/image1_2.jpg";
+       }
+function changePic22(){
+       document.querySelector("#image1").src = "images/image1.jpg";
+       }
