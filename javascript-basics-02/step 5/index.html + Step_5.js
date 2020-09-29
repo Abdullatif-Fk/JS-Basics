3ebe5ac09317495b857eaa2176d45cdf21506874
Abdullatif-Fk <abul647.fk@gmail.com>
2020-09-29 17:49:03 +0300

    basic02 step-5

---
+++ javascript-basics-02/step 5/index.html + Step_5.js
@@ -0,0 +1,30 @@
+function chagePic(){
+   var image1= document.getElementById("image1");
+   image1.onmouseover=function () {
+       image1.src="images/image1_2.jpg";
+       
+   }
+   var image2= document.getElementById("image2");
+   image2.onmouseover=function () {
+       image2.src="images/image2_2.jpg";
+       
+   }
+   var image3= document.getElementById("image3");
+   image3.onmouseover=function () {
+       image3.src="images/image3_2.jpg";
+       
+   }
+   var image4= document.getElementById("image4");
+   image4.onmouseover=function () {
+       image4.src="images/image4_2.jpg";
+       
+   }
+   var image5= document.getElementById("image5");
+   image5.onmouseover=function () {
+       image5.src="images/image5_2.jpg";
+       
+   }
+
+
+
+}
